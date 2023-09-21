77c631f07273caaa028b541ad028dc6749979ceb
Alex <vygnicholeksandrr@gmail.com>
2023-09-21 14:18:52 +0300

    collection evens

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -33,6 +33,7 @@ if (!customElements.get('media-gallery')) {
         this.elements.viewer.querySelectorAll('[data-media-id]').forEach((element) => {
           element.classList.remove('is-active');
         });
+        console.log(activeMedia)
         activeMedia.classList.add('is-active');
 
         if (prepend) {
